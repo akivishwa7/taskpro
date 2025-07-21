0b183b1962c699e409ea313eda2f9dae9df1eaa8
ankit <akivishwa7@gmail.com>
2025-07-21 17:07:02 +0530

    1st commit

---
+++ src/main/java/com/taskpro/TaskProApplication.java
@@ -0,0 +1,11 @@
+package com.taskpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskProApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskProApplication.class, args);
+    }
+}
